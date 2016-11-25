e4398540a941f0adbad1de5ac35a78722e01a797
lungd <lungdm@gmail.com>
2016-11-25 21:43:17 +0000

    UpdatedSpreadsheetDataReader now reads CSV file

--- CElegans/pythonScripts/c302/UpdatedSpreadsheetDataReader.py
+++ CElegans/pythonScripts/c302/UpdatedSpreadsheetDataReader.py
@@ -2,7 +2,7 @@
 
 ############################################################
 
-#    A simple script to read the values in CElegansNeuronTables.xls.
+#    A simple script to read the values in herm_full_edgelist.csv.
 
 #    Note: this file will be replaced with a call to PyOpenWorm
 #    when that package is updated to read all of this data from the 
@@ -10,10 +10,9 @@
 
 ############################################################
 
+import csv
 
 from NeuroMLUtilities import ConnectionInfo
-
-from xlrd import open_workbook
 import os
 
 spreadsheet_location = os.path.dirname(os.path.abspath(__file__))+"/../../../"
@@ -22,29 +21,28 @@ def readDataFromSpreadsheet(include_nonconnected_cells=False):
 
     conns = []
     cells = []
-    filename = "%sherm_full_edgelist.xls"%spreadsheet_location
-    rb = open_workbook(filename)
+    filename = "%sherm_full_edgelist.csv" % spreadsheet_location
+    
+    with open(filename, 'rb') as f:
+        reader = csv.DictReader(f)
+        print "Opened file: " + filename
 
-    print "Opened Excel file: " + filename
+        known_nonconnected_cells = ['CANL', 'CANR', 'VC6']
 
-    known_nonconnected_cells = ['CANL', 'CANR', 'VC6']
+        for row in reader:
+            pre = str.strip(row["Source"])
+            post = str.strip(row["Target"])
+            num = int(row["Weight"])
+            syntype = str.strip(row["Type"])
+            synclass = 'Generic_GJ' if 'electrical' in syntype else 'Chemical_Synapse'
 
+            conns.append(ConnectionInfo(pre, post, num, syntype, synclass))
+            if pre not in cells:
+                cells.append(pre)
+            if post not in cells:
+                cells.append(post)
 
-    for row in range(1,rb.sheet_by_index(0).nrows):
-        pre = str.strip(str(rb.sheet_by_index(0).cell(row,0).value))
-        post = str.strip(str(rb.sheet_by_index(0).cell(row,1).value))
-        num = int(rb.sheet_by_index(0).cell(row, 2).value)
-        syntype = rb.sheet_by_index(0).cell(row,3).value
-        synclass = 'Generic_GJ' if 'electrical' in syntype else 'Chemical_Synapse'
-        #print "%s-%s %s %s %s"%(pre,post,num,syntype,synclass)
-
-        conns.append(ConnectionInfo(pre, post, num, syntype, synclass))
-        if pre not in cells:
-            cells.append(pre)
-        if post not in cells:
-            cells.append(post)
-
-    if include_nonconnected_cells:
-        for c in known_nonconnected_cells: cells.append(c)
+        if include_nonconnected_cells:
+            for c in known_nonconnected_cells: cells.append(c)
 
     return cells, conns
